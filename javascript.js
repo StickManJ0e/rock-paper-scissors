96be87a5f30e2e8d0d69e9fe5a4446283170feba
StickManJ0e <121269062+StickManJ0e@users.noreply.github.com>
2023-01-01 12:38:28 +1100

    Fix functions

--- javascript.js
+++ javascript.js
@@ -1,67 +1,60 @@
-//Calls function to gte users input and then converts to lower case
-let playerSelection = (playerChoice) => (prompt("Select 'Rock', 'Paper' or 'Scissors: ")).toLowerCase();
-
 //Create list of possible choices and calls function to make computer randomly return one
 const choiceList = ["rock", "paper", "scissors"];
+let computerSelection;
 function getComputerChoice(choiceList) {
-    return (computerSelection = choiceList[Math.floor(Math.random() * choiceList.length)]);
+    let computerChoice = choiceList[Math.floor(Math.random() * choiceList.length)];
+    computerSelection = (computerChoice).toString();
+    return computerSelection;
 }
-let computerSelection = getComputerChoice;
 
 
+//Calls function to gte users input and then converts to lower case
+let playerSelection;
+function getPlayerChoice() {
+    let choice = prompt("Select 'Rock', 'Paper' or 'Scissors: ");
+    let convertedChoice = choice.toLowerCase();
+    playerSelection = (convertedChoice).toString();
+    return playerSelection;
+}
+
+    
 //Score System
 let playerScore = 0;
-// let computerScore = 0;
-// let currentScore = (score) => console.log(`You: ${playerScore} | Computer: ${computerScore}`);
+let computerScore = 0;
+let currentScore = (score) => console.log(`You: ${playerScore} | Computer: ${computerScore}`);
 
 //Plays one round where it returns the result of the round
-function playRound(playerSelection, computerSelection) {
-    console.log(playerSelection());
+function playRound() {
+    console.log(getPlayerChoice());
     console.log(getComputerChoice(choiceList));
-    switch (playerSelection, computerSelection) {
-        case ("rock", "rock"):
-            return ("It's a Tie!");
-            break;
-        case ("rock", "paper"):
-            return ("You Lose! Paper beats Rock!");
-            computerScore++;
-            break;
-        case ("rock", "scissors"):
-            return ("You Win! Rock beats Scissors!");
-            playerScore++;
-            break;
-        case ("paper", "rock"):
-            return("You Win! Paper beats Rock!");
-            playerScore++;
-            break;
-        case ("paper", "paper"):
-            return("It's a Tie!");
-            break;
-        case ("paper", "scissors"):
-            return("You Lose! Scissors beats Paper!");
-            computerScore++;
-            break;
-        case ("scissors", "rock"):
-            return("You Lose! Rock beats Scissors!");
-            computerScore++;
-            break;
-        case ("scissors", "paper"):
-            return("You Win! Scissors beats Paper!");
-            playerScore++;
-            break;
-        case ("scissors", "scissors"):
-            return("It's a Tie!");
-            break;
-        default:
-            return("Invalid Input");
-    }
+    console.log(playerSelection, computerSelection);
+
+   if (playerSelection === computerSelection) {
+    return ("It's a tie");
+   }
+   else if (playerSelection === "rock" && computerSelection === "paper") {
+    return ("You Lose! Paper beats Rock!");
+   }
+   else if (playerSelection === "paper" && computerSelection === "scissors") {
+    return ("You Lose! scissors beats Paper!");
+   }
+   else if (playerSelection === "scissors" && computerSelection === "rock") {
+    return ("You Lose! Rock beats scissors!");
+   }
+   else if (playerSelection === "rock" && computerSelection === "scissors") {
+    return ("You Win! Rock beats Scissors!");
+   }
+   else {
+    return ("Invalid Input")
+   }
+
 }
 
 
 function game() {
     for (let i = 0; i < 5; i++) {
-         console.log(`Round ${i+1}: ` + playRound(playerSelection, computerSelection));
-         //currentScore();
+         console.log(`Round ${i+1}: ` + playRound());
+         currentScore();
      }
  }
 
